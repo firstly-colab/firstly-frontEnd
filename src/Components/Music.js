862db6a6d0aa7071c729b2bfc2715a24f13522e7
Gigi <yuengepoon@gmail.com>
2022-08-14 15:49:58 -0400

    finished the music page

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -2,8 +2,17 @@ import Illustration2 from "../assets/Illustration2.svg"
 import { useNavigate } from "react-router-dom";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { IconButton } from "@mui/material";
+import PlayCircleIcon from '@mui/icons-material/PlayCircle'; 
+import { useState } from "react";
 
 const Music = () => {
+
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal);
+    };
+
     const navigate = useNavigate();
 
     const handleSubmit = () => {
@@ -26,8 +35,31 @@ const Music = () => {
                 <p>Remember to just have fun... You have nothing to lose!</p>
                 <img src={Illustration2} alt="an illustration of a male and female head"></img>
                 <p>Here’s a song to pump you up:</p>
-                <a href="https://www.spotify.com">Link to spotify</a>
-                <button onClick={handleSubmit}>Back home</button>
+
+
+                <button
+                    className="musicButton" 
+                    onClick={toggleModal}>
+                    <PlayCircleIcon className="playlist"/>
+                    <span className="flexCont">
+                        Confidence Boost
+                        <span className="spotify">Spotify</span>
+                    </span>
+                </button>
+
+
+
+                <button className="submit" onClick={handleSubmit}>Back home</button>
+
+                {modal && (
+                    <div className="modal">
+                        <div onClick={toggleModal} className="overlay"></div>
+                        <div className="modalContent">
+                            <h3>Listen to Confidence Boost?</h3>
+                            <a href="https://www.spotify.com">Open Spotify</a>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
