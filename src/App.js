3904db7550d9847951bb92594fa23924a6d900cd
Gurupriyan <gurupriyan98@gmail.com>
2022-08-16 01:06:18 -0400

    Added login and signup functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Survey from './Components/Survey';
 import Results from './Components/Results';
 import Music from './Components/Music';
 import Dashboard from "./Components/Dashboard";
+import SignupPage from './Components/SignupPage'
 
 //styles
 import './style/master.css'
@@ -17,6 +18,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path='/signup' element={<SignupPage />} />
           <Route path="/survey" element={<Survey />} />
           <Route path="/survey/results" element={<Results />} />
           <Route path="/survey/music" element={<Music />} />
---
+++ src/Components/SignupPage.js
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+import { useNavigate } from "react-router-dom";
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+
+
+const SignupPage = () => {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [message, setMessage] = useState('')
+
+    const navigate = useNavigate()
+    async function register () {
+		const response = await fetch('http://localhost:3001/register', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				email,
+                name,
+				password
+			})
+		})
+
+        const data = await response.json();
+
+        if (!data.token) {
+            setMessage(data)
+            return
+        }
+        navigate('/login')
+    }
+
+    function handleSubmit (e) {
+        e.preventDefault()
+        register()
+    }
+
+
+    return (
+        <div className="signup">
+			<div className="wrapper">
+				<h2>Sign up to get started!</h2>
+				<p>One step closer to curated conversation starters for your first date.</p>
+				<form>
+                    <label>
+						Name
+						<input 
+							type="text" 
+							name="Name" 
+							placeholder="Enter your first name" 
+							onChange={(event) => setName(event.target.value)}
+							value = {name}
+						/>
+					</label>
+					<label>
+						Email
+						<input 
+							type="text" 
+							name="Email" 
+							placeholder="Enter your email" 
+							onChange={(event) => setEmail(event.target.value)}
+							value = {email}
+						/>
+					</label>
+					<label>
+						Password
+						<input 
+							type="password" 
+							name="Password" 
+							placeholder="Enter your password"
+							onChange={(event) => setPassword(event.target.value)}
+							value = {password}
+							/>
+						<VisibilityOffIcon />
+					</label>
+					{message && <p> {message} </p>}
+					<button onClick={handleSubmit}>Sign up</button>
+				</form>
+				<p>Already have an account? <a href= "/login"> Log in </a></p>
+			</div>
+		</div>
+    )
+}
+
+export default SignupPage;
